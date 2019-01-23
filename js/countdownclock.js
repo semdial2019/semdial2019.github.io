9c3f5b4c95a6537b679a10b4349c631298f45eaf
Janosch Haber <j.haber@qmul.ac.uk>
2019-01-23 15:54:10 +0000

    Design Update v0.2
    Reduced header size and display logistics +++ Combined header and countdown +++ Added top-bar logo

--- js/countdownclock.js
+++ js/countdownclock.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date('September 10, 2019 09:00:00');
+var deadline = new Date('September 4, 2019 09:00:00');
 initializeClock('clockdiv', deadline);
